1cafebf5ad05aa478473d28ff53e0db64f00f96e
Rananjaya <rananjaya@tsdclanka.com>
2020-10-11 14:56:07 +0530

    api run test

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req, res) => {
-    res.send("<h1>Home Page/h1>")
+    res.send("<h1>Home Page</h1>")
 })
 
 app.listen(5000, () =>{
-     console.log("Server dtarted on Port 5000");
+     console.log("Server started on Port 5000");
 })
