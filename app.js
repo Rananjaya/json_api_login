212066641198fb90770eaff1b196b2193d945a9e
Rananjaya <rananjaya@tsdclanka.com>
2020-10-11 14:48:34 +0530

    API_step1

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req, res) => {
+    res.send("<h1>Home Page/h1>")
+})
+
+app.listen(5000, () =>{
+     console.log("Server dtarted on Port 5000");
+})
